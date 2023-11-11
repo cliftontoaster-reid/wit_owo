76e3cf5668ad227ff12534282aebd83af18e0327
Clifton Toaster Reid <42320331+cliftontoaster-reid@users.noreply.github.com>
2023-11-11 16:39:29 +0100

    Added multilingual.

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,11 +1,13 @@
 pub use crate::model::client::Client;
+#[cfg(feature = "lingua")]
+pub use crate::model::client::MultiLingualClient;
 pub use crate::model::dictation::{DictationResponse, FullDictationResponse};
 pub use crate::model::message::{Entity, Intent, Message};
+#[cfg(feature = "server")]
+pub use crate::model::server::prelude::*;
 pub use crate::model::speech::{
   AudioContentType, FullSpeechResponse, HalfSpeechResponse, SpeechRequest, SpeechResponse,
 };
 pub use crate::model::synthesize::SynthesizeRequest;
 pub use crate::model::voices::*;
 pub use crate::model::{DynamicEntities, DynamicEntity, WitError};
-#[cfg(feature = "server")]
-pub use crate::model::server::prelude::*;
